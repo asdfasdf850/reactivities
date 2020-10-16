94cdfc49dcbcc1c31cff9852f18a6247e8de588a
asdfasdf850 <menyazovutartem9@gmail.com>
2020-10-16 15:31:33 +0300

    end of section 14

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Application.Activities;
 using Application.Interfaces;
 using API.Middleware;
+using AutoMapper;
 using Domain;
 using FluentValidation.AspNetCore;
 using Infrastructure.Security;
@@ -21,32 +22,43 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
-namespace API {
-   public class Startup {
-      public Startup(IConfiguration configuration) {
+namespace API
+{
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
          Configuration = configuration;
       }
 
       public IConfiguration Configuration { get; }
 
-      public void ConfigureServices(IServiceCollection services) {
-         services.AddDbContext<DataContext>(opt => {
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddDbContext<DataContext>(opt =>
+         {
+            opt.UseLazyLoadingProxies();
             opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
          });
 
-         services.AddCors(opt => {
-            opt.AddPolicy("CorsPolicy", policy => {
+         services.AddCors(opt =>
+         {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
             });
          });
 
          services.AddMediatR(typeof(List.Handler).Assembly);
+         services.AddAutoMapper(typeof(List.Handler));
 
-         services.AddControllers(opt => {
+         services.AddControllers(opt =>
+            {
                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
                opt.Filters.Add(new AuthorizeFilter(policy));
             })
-            .AddFluentValidation(cfg => {
+            .AddFluentValidation(cfg =>
+            {
                cfg.RegisterValidatorsFromAssemblyContaining<Create>();
             });
 
@@ -55,11 +67,23 @@ namespace API {
          identityBuiler.AddEntityFrameworkStores<DataContext>();
          identityBuiler.AddSignInManager<SignInManager<AppUser>>();
 
+         services.AddAuthorization(opt =>
+         {
+            opt.AddPolicy("IsActivityHost", policy =>
+            {
+               policy.Requirements.Add(new IsHostRequirement());
+            });
+         });
+
+         services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+
          var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
 
          services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(opts => {
-               opts.TokenValidationParameters = new TokenValidationParameters {
+            .AddJwtBearer(opts =>
+            {
+               opts.TokenValidationParameters = new TokenValidationParameters
+               {
                ValidateIssuerSigningKey = true,
                IssuerSigningKey = key,
                ValidateAudience = false,
@@ -71,9 +95,11 @@ namespace API {
          services.AddScoped<IUserAccessor, UserAccessor>();
       }
 
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
          app.UseMiddleware<ErrorHandlingMiddleware>();
-         if (env.IsDevelopment()) {
+         if (env.IsDevelopment())
+         {
             // app.UseDeveloperExceptionPage();
          }
          //  app.UseHttpsRedirection();
@@ -81,7 +107,8 @@ namespace API {
          app.UseCors("CorsPolicy");
          app.UseAuthentication();
          app.UseAuthorization();
-         app.UseEndpoints(endpoints => {
+         app.UseEndpoints(endpoints =>
+         {
             endpoints.MapControllers();
          });
       }
